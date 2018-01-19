d36da60091c339234263cd24ece65b4805fcbb1b
Tim <lwpatent@yahoo.com>
2018-01-19 14:46:39 -0500

    about to work on view

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
   @@URL_BASE="htp://thecatapi.com/api/"
   apikey = "MjM5MTGY"
+
+  #add this to cat.rb
+
+  	def self.get_cats
+  		url = @@url_base + "/images/get?format=xml&results_per_page=40"
+  		return HTTParty.get(url).parsed_response["response"]["data"]["images"]["image"]
+  	end
+
+
 end
